f4440d1f46b362855f6ee8ee7b795a379efabf0e
Sun Lin <sun.lin5403@gmail.com>
2020-03-03 16:48:35 -0500

    Exercise 3.7 Solution

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './styles.css';
+
+class App extends React.Component {
+  backgroundColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      className : 'red'
+    };
+  }
+
+  changeBackgroundColor() {
+    let rand = Math.random()
+    this.setState({
+      className : this.backgroundColors[Math.floor(rand * this.backgroundColors.length)]
+    })
+  }
+
+  render() {
+    return (
+      <div className={this.state.className} onClick={() => this.changeBackgroundColor()}>Click me to change color!</div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
